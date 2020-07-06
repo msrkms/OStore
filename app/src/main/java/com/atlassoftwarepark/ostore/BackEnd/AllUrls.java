3cb8132f2931ce8bb825025cbb4478d06312fe43
Sajidur Rahman <sajidur35-1831@diu.edu.bd>
2020-07-07 03:17:12 +0600

    Login API Connected

---
+++ app/src/main/java/com/atlassoftwarepark/ostore/BackEnd/AllUrls.java
@@ -0,0 +1,9 @@
+package com.atlassoftwarepark.ostore.BackEnd;
+
+public class AllUrls {
+    private static  final String Domain="http://ostore.xyz";
+    private static String API=Domain+"/api";
+
+    public static final String Login=API+"/submitlogin";
+
+}
